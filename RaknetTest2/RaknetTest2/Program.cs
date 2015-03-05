a916b949580418038e7d52e929ed1f9690b652ec
Siroro <medasir@gmail.com>
2015-03-05 20:50:09 +0000

    Complete demo

--- RaknetTest2/RaknetTest2/Program.cs
+++ RaknetTest2/RaknetTest2/Program.cs
@@ -15,9 +15,10 @@ namespace RaknetTest2
             BitStream stringTestSendBitStream = new BitStream();
             BitStream rakStringTestSendBitStream = new BitStream();
             BitStream receiveBitStream = new BitStream();
-
+            String holdingString;
             TimeSpan startTimeSpan;
             RakString rakStringTest = new RakString();
+
             RakPeerInterface testClient = RakPeer.GetInstance();
 
             testClient.Startup(1, new SocketDescriptor(60000, "127.0.0.1"), 1);
@@ -26,13 +27,15 @@ namespace RaknetTest2
             testServer.Startup(1, new SocketDescriptor(60001, "127.0.0.1"), 1);
             testServer.SetMaximumIncomingConnections(1);
 
+            Console.WriteLine("Send and receive loop using BitStream.\nBitStream read done into RakString");
+
             testClient.Connect("127.0.0.1", 60001, "", 0);
 
             String sendString = "The test string";
             stringTestSendBitStream.Write((byte)DefaultMessageIDTypes.ID_USER_PACKET_ENUM);
             stringTestSendBitStream.Write(sendString);
 
-            RakString testRakString = new RakString("Test packet received ");
+            RakString testRakString = new RakString("Test RakString");
             rakStringTestSendBitStream.Write((byte)DefaultMessageIDTypes.ID_USER_PACKET_ENUM);
             rakStringTestSendBitStream.Write(testRakString);
 
@@ -48,13 +51,46 @@ namespace RaknetTest2
                     receiveBitStream.Write(testPacket.data, testPacket.length);
                     receiveBitStream.IgnoreBytes(1);
                     receiveBitStream.Read(rakStringTest);
-                    Console.WriteLine("Packets received: " + loopNumber + "\nData: " + rakStringTest.C_String());
+                    Console.WriteLine("Loop number: " + loopNumber + "\nData: " + rakStringTest.C_String());
                 }
                 testServer.DeallocatePacket(testPacket);
                 loopNumber++;
                 System.Threading.Thread.Sleep(50);
                 testClient.Send(rakStringTestSendBitStream, PacketPriority.LOW_PRIORITY, PacketReliability.RELIABLE_ORDERED, (char)0, new AddressOrGUID(new SystemAddress("127.0.0.1", 60001)), false);
             }
+
+            Console.WriteLine("String send and receive loop using BitStream.\nBitStream read done into String");
+
+            SystemAddress[] remoteSystems;
+            ushort numberOfSystems = 1;
+            testServer.GetConnectionList(out remoteSystems, ref numberOfSystems);
+
+            startTimeSpan = (DateTime.UtcNow - new DateTime(1970, 1, 1));
+            loopNumber = 0;
+            while (startTimeSpan.TotalSeconds + 5 > (DateTime.UtcNow - new DateTime(1970, 1, 1)).TotalSeconds)
+            {
+                testPacket = testServer.Receive();
+                if (testPacket != null && testPacket.data[0] == (byte)DefaultMessageIDTypes.ID_USER_PACKET_ENUM)
+                {
+                    receiveBitStream.Reset();
+                    receiveBitStream.Write(testPacket.data, testPacket.length);
+                    receiveBitStream.IgnoreBytes(1);
+                    receiveBitStream.Read(out holdingString);
+                    Console.WriteLine("Loop number: " + loopNumber + "\nData: " + holdingString);
+                }
+                testServer.DeallocatePacket(testPacket);
+                loopNumber++;
+                System.Threading.Thread.Sleep(50);
+                SystemAddress sa = RakNet.RakNet.UNASSIGNED_SYSTEM_ADDRESS;
+                testClient.Send(stringTestSendBitStream, PacketPriority.LOW_PRIORITY, PacketReliability.RELIABLE_ORDERED, (char)0, new AddressOrGUID(new SystemAddress("127.0.0.1", 60001)), false);
+            }
+            //If RakString is not freed before program exit it will crash
+            rakStringTest.Dispose();
+            testRakString.Dispose();
+
+            RakPeer.DestroyInstance(testClient);
+            RakPeer.DestroyInstance(testServer);
+            Console.WriteLine("Demo complete. Press Enter.");
             Console.Read();
 
         }
